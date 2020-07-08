e707ff9dfb4a2800e51d836d4fb0f0689b87f8b6
Strelok-SV <streloksv036@gmail.com>
2020-07-08 17:10:25 +0300

    add_main_menu

--- javascript/time.js
+++ javascript/time.js
@@ -1,14 +1,14 @@
-trueFormat = value => { return value < 10 ? value = '0' + value : value }
+formatTrue = value => { return value < 10 ? value = '0' + value : value }
 
 function dateTime() {
     let date = new Date()
-    let minutes = trueFormat(date.getMinutes())
-    let hours = trueFormat(date.getHours())
-    let day = trueFormat(date.getDate())
-    let month = trueFormat(date.getMonth() + 1)
+    let minutes = formatTrue(date.getMinutes())
+    let hours = formatTrue(date.getHours())
+    let day = formatTrue(date.getDate())
+    let month = formatTrue(date.getMonth() + 1)
     let year = date.getFullYear()
 
     return `${hours}:${minutes} ${day}/${month}/${year}`
 }
 
-setInterval(() => document.getElementById("data_time").innerHTML = dateTime(), 1000)
+setInterval(() => document.getElementsByClassName("date_time")[0].innerHTML = dateTime(), 1000)
