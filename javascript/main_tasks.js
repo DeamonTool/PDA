26f0c2ec807de2b53576fd7b685239ffed7936b8
Strelok-SV <streloksv036@gmail.com>
2020-07-22 18:12:29 +0300

    add_scroll

--- javascript/main_tasks.js
+++ javascript/main_tasks.js
@@ -1,5 +1,20 @@
-function barLeftMenuSwitch(index) {
-    let list = document.getElementById("bar_left_menu_list").getElementsByTagName("li")
+function tasksLeftMenuSwitch(index) {
+    let list_menu = document.getElementById("tasks_left_menu_list").getElementsByTagName("li")
+    let list = [
+        document.getElementsByClassName("tasks_left_menu_active"),
+        document.getElementsByClassName("tasks_left_menu_completed"),
+        document.getElementsByClassName("tasks_left_menu_failed")
+    ]
 
-    for (let i = 0; i < 3; i++) i == index ? list[i].style.color = "rgb(230, 160, 0)" : list[i].style.color = ""
+    for (let i = 0; i < 3; i++) {
+        if (i == index) {
+            list_menu[i].style.color = "rgb(230, 160, 0)"
+            list[i].style = "display: block"
+        }
+        else {
+            list_menu[i].style.color = ""
+            list[i].style = "display: none"
+        }
+    }
+
 }
